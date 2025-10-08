7d0bb3fc6b0b307bc244e4d156f69db09ec9f1aa
Chandan Kumar Mandal <chandan_kumarmandal@apple.com>
2025-10-08 16:34:05 +0530

    setup react query
    Signed-off-by: Chandan Kumar Mandal <chandan_kumarmandal@apple.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import queryClient from "@/utils/queryClient";
 import "./tailwind.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<App />} />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
